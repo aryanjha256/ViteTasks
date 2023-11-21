f7b1af380a481c14d777fa8cdc5d9d9a1a9be1b8
Aryan Kumar <aryanajha256@gmail.com>
2023-11-22 01:24:07 +0530

    color update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import React from "react";
 import "./App.css";
+import Login from "./components/Login";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-background text-text">
+    <div className="min-h-screen bg-background text-text dark">
       <div id="handle">
-        <h1 className="font-heading">App</h1>
+        <h1 className="text-2xl text-primary">Your Tasks</h1>
+        <h1 className="text-lg text-secondary">Manage Your Tasks</h1>
+        <Login />
       </div>
     </div>
   );
---
+++ src/components/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <div>
+      <h1>Login Here</h1>
+    </div>
+  );
+};
+
+export default Login;
